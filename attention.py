680d81fdbc12262794da9f381855471ea7cdb397
Pratik Ghimire <ghimirepratik039@gmail.com>
2026-09-24 20:51:46 +0545

    remove all useless fuction and make only one clean function

--- attention.py
+++ attention.py
@@ -1,48 +1,41 @@
 import numpy as np
-def create_qkv(x,w_q,w_k,w_v):
+def self_attention(x,w_q,w_k,w_v):
+    #creating q k v
     q=np.dot(x,w_q)
     k=np.dot(x,w_k)
     v=np.dot(x,w_v)
-    return q , k, v
-def attention_score(q,k):
+    #attention score 
     score=np.dot(q,k.T)
-    return score
-def scaled_attention_score(score,d_k):
-    scaled_attention=score/np.sqrt(d_k)
-    return scaled_attention
-def softmax(x):
-    x=x-np.max(x,axis=1,keepdims=True)
+    #scaling 
+    q_k=k.shape[-1]
+    scaled_score=score/np.sqrt(q_k)
+    #applying softmax to the manual transformer 
+    x=scaled_score - np.max(scaled_score,axis=1,keepdims=True)
     x_exp=np.exp(x)
-    return x_exp/np.sum(x_exp,axis=1,keepdims=True)
-def attention_weights(weight,v):
-    return np.dot(weight,v)
-if __name__ =="__main__":
-    x=np.array([
-        [1.0, 0.0, 1.0, 0.0],
-        [0.0, 2.0, 0.0, 1.0],
-        [1.0, 1.0, 0.0, 2.0]
-    ])
-    w_q=np.array([
-       [1.0,0.0],
-       [0.0,1.0],
-       [1.0,0.0],
-       [0.0,1.0] 
-    ])
-    #for now we are creating same as for the finding reasoning so we create same for temporary
-    w_k=w_q
-    w_v=w_q
-    q,k,v=create_qkv(x,w_q,w_k,w_v)
-    score=attention_score(q,k)
-    d_k=k.shape[-1]
-    softmaxs=softmax(score)
-    print("Softmax is ",softmaxs)
-    
-    scaled_score=scaled_attention_score(score,d_k)
-    print("Scaled attention Score is ",scaled_score)
-    attention_weight=attention_weights(softmaxs,v)
-    print("Attention weight is ",attention_weight)
-    print("Score is ",score)
-    print("Q is ",q)
-    print("K is ",k) 
-    print("V is ",v)
-    
+    softmax=x_exp/np.sum(x_exp,axis=1,keepdims=True)
+    #applying attention weight 
+    output=np.dot(softmax,v)
+    print("Output of the attention is \n",output)
+    return output
+w_q = np.array([
+    [1.0, 0.0],
+    [0.0, 1.0],
+    [1.0, 0.0],
+    [0.0, 1.0]
+])
+
+w_k = np.array([
+    [0.5, 1.0],
+    [1.0, 0.5],
+    [0.5, 0.5],
+    [1.0, 1.0]
+])
+
+w_v = np.array([
+    [1.0, 0.5],
+    [0.5, 1.0],
+    [1.0, 1.0],
+    [0.5, 0.5]
+])
+x=np.array([ [1.0, 0.0, 1.0, 0.0], [0.0, 2.0, 0.0, 1.0], [1.0, 1.0, 0.0, 2.0] ])
+self_attention(x,w_q,w_k,w_v)
